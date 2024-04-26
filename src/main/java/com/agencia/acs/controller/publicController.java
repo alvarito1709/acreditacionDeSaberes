93174d3e2202440fbf45f4777ab7828ea177df8c
Alvaro <alvaro9943310@gmail.com>
2024-04-26 14:09:38 -0300

    visualización de menu lateral, cambios esteticos en el login

--- src/main/java/com/agencia/acs/controller/publicController.java
+++ src/main/java/com/agencia/acs/controller/publicController.java
@@ -41,4 +41,5 @@ public class publicController {
             //}
         return "common";
     }
+    
 }
---
+++ src/main/resources/static/javaScript/common.js
@@ -0,0 +1,18 @@
+
+const listaDesplegable = document.getElementById("listaEditables");
+const contenedores = document.getElementsByClassName("itemContainer");
+const lista = document.getElementsByClassName("listItem");
+
+function desplegarLista(){
+
+    if (listaDesplegable.style.display === 'block' || listaDesplegable.style.display === ''){
+        listaDesplegable.style.transition = 'height 1s ease'
+        listaDesplegable.style.display = 'none';
+    }else{
+        listaDesplegable.style.transition = 'height 1s ease'
+        listaDesplegable.style.display = 'block';
+    }
+}
+
+
+
