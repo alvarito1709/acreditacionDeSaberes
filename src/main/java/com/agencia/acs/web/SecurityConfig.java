645a713b2359f6d22d59a9dc781432755dd3643a
Alvaro <alvaro9943310@gmail.com>
2024-05-26 22:47:41 -0300

    pagina de info lista

--- src/main/java/com/agencia/acs/web/SecurityConfig.java
+++ src/main/java/com/agencia/acs/web/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/common")
+                .defaultSuccessUrl("/board")
                 .and()
                 .logout()
                 .logoutUrl("/logout")
