492ca73ec21a9cc372bf8d88003403d341a6f388
Alvaro <alvaro9943310@gmail.com>
2024-08-05 22:14:04 -0300

    flujo de test

--- src/main/java/com/agencia/acs/controller/InscripcionController.java
+++ src/main/java/com/agencia/acs/controller/InscripcionController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -30,5 +31,17 @@ public class InscripcionController {
     @GetMapping("/modal")
     public ModelAndView modal(){
         return new ModelAndView("inscripcionATrayectos::modalDeInscripcion");
+    }
+
+    @GetMapping("/vistaCuestionario")
+    public ModelAndView vistaCuestionario(Model model){
+        return new ModelAndView("inscripcionATrayectos::vistaCuestionario");
+    }
+
+    @GetMapping("/iniciarCuestionario")
+    public  ModelAndView iniciarCuestionario(@RequestParam(value = "trayecto") String trayecto){
+
+       return new ModelAndView("testModulos::"+trayecto);
+
     }
 }
--- src/main/resources/static/javaScript/inscripcion.js
+++ src/main/resources/static/javaScript/inscripcion.js
@@ -1,11 +1,15 @@
 //const url = "http://localhost:8080/";
 
 
+let trayectoId = "";
+
+
+//ESTA FUNCION ESTA HARDCODEADA
 function buscarTrayectos(){
 
     const selectSector = document.getElementById("sectorSeleccionado");
 
-    $.ajax({
+  /*  $.ajax({
         type:'POST',
         url: url + "trayectos/buscarTrayectosPorSector",
         data: {sectorId:selectSector.value},
@@ -14,19 +18,200 @@ function buscarTrayectos(){
         }]
     })
 
+   */
+
+    switch (selectSector.value){
+        case 'Gastronomia':
+            $('.gastronomia').each(function() {
+                $(this).css('display', 'block');
+            });
+            $('.energiaElectrica').each(function() {
+                $(this).css('display', 'none');
+            });
+            $('.construcciones').each(function() {
+                $(this).css('display', 'none');
+            });
+            $('.informatica').each(function() {
+                $(this).css('display', 'none');
+            });
+            $('.mecanicaDeMotos').each(function() {
+                $(this).css('display', 'none');
+            });
+            $('.mecanicaAutomotriz').each(function() {
+                $(this).css('display', 'none');
+            });
+            $('.reparacionYMantenimientoDeMáquinasYEquiposElectricosElectronicos').each(function() {
+                $(this).css('display', 'none');
+            });
+
+            break;
+
+        case 'Energía Eléctrica':
+            $('.gastronomia').each(function() {
+                $(this).css('display', 'none');
+            });
+            $('.energiaElectrica').each(function() {
+                $(this).css('display', 'block');
+            });
+            $('.construcciones').each(function() {
+                $(this).css('display', 'none');
+            });
+            $('.informatica').each(function() {
+                $(this).css('display', 'none');
+            });
+            $('.mecanicaDeMotos').each(function() {
+                $(this).css('display', 'none');
+            });
+            $('.mecanicaAutomotriz').each(function() {
+                $(this).css('display', 'none');
+            });
+            $('.reparacionYMantenimientoDeMáquinasYEquiposElectricosElectronicos').each(function() {
+                $(this).css('display', 'none');
+            });
+            break;
+
+            case 'Construcciones':
+                $('.gastronomia').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.energiaElectrica').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.construcciones').each(function() {
+                    $(this).css('display', 'block');
+                });
+                $('.informatica').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.mecanicaDeMotos').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.mecanicaAutomotriz').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.reparacionYMantenimientoDeMáquinasYEquiposElectricosElectronicos').each(function() {
+                    $(this).css('display', 'none');
+                });
+            break;
+
+            case 'Mecánica Automotriz':
+                $('.gastronomia').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.energiaElectrica').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.construcciones').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.informatica').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.mecanicaDeMotos').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.mecanicaAutomotriz').each(function() {
+                    $(this).css('display', 'block');
+                });
+                $('.reparacionYMantenimientoDeMáquinasYEquiposElectricosElectronicos').each(function() {
+                    $(this).css('display', 'none');
+                });
+            break;
+
+            case 'Mecánico de Motos':
+                $('.gastronomia').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.energiaElectrica').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.construcciones').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.informatica').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.mecanicaDeMotos').each(function() {
+                    $(this).css('display', 'block');
+                });
+                $('.mecanicaAutomotriz').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.reparacionYMantenimientoDeMáquinasYEquiposElectricosElectronicos').each(function() {
+                    $(this).css('display', 'none');
+                });
+            break;
+
+            case 'Reparación y mantenimiento de máquinas y equipos eléctricos - electrónicos':
+                $('.gastronomia').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.energiaElectrica').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.construcciones').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.informatica').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.mecanicaDeMotos').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.mecanicaAutomotriz').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.reparacionYMantenimientoDeMáquinasYEquiposElectricosElectronicos').each(function() {
+                    $(this).css('display', 'block');
+                });
+            break;
+
+            case 'Informática':
+                $('.gastronomia').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.energiaElectrica').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.construcciones').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.informatica').each(function() {
+                    $(this).css('display', 'block');
+                });
+                $('.mecanicaDeMotos').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.mecanicaAutomotriz').each(function() {
+                    $(this).css('display', 'none');
+                });
+                $('.reparacionYMantenimientoDeMáquinasYEquiposElectricosElectronicos').each(function() {
+                    $(this).css('display', 'none');
+                });
+            break;
+    }
+
 }
 
 function mostrarModalInscripcion(){
 
+    const trayectoSeleccionado = document.getElementById("trayectoSeleccionado");
+
     const modalContainer = document.getElementById("modalesParaAgregarContainer");
-    $.ajax({
-        type:'GET',
-        url: url + "inscripcion/modal",
-        success: [function (response){
-            $("#modalesParaAgregarContainer").html(response);
-            modalContainer.style.display = "flex";
-        }]
-    })
+
+    if (trayectoSeleccionado.value === ""){
+        alert("Seleccione un trayecto")
+    }else {
+        $.ajax({
+            type:'GET',
+            url: url + "inscripcion/modal",
+            success: [function (response){
+                $("#modalesParaAgregarContainer").html(response);
+                modalContainer.style.display = "flex";
+            }]
+        })
+    }
+
 }
 
 function ocultarModalInscripcion(){
@@ -35,6 +220,39 @@ function ocultarModalInscripcion(){
     modalContainer.style.display = "none";
 }
 
+
+function llevarAlCuestionario(){
+
+    const modalContainer = document.getElementById("modalesParaAgregarContainer");
+
+    trayectoId = document.getElementById("trayectoSeleccionado").value;
+
+    $.ajax({
+        type:'GET',
+        url: url + "inscripcion/vistaCuestionario",
+        success: [function (response){
+            $("#tableContainer").html(response);
+            modalContainer.style.display = "none";
+        }]
+    })
+
+}
+
+function mostrarCuestionario(){
+
+    const modalContainer = document.getElementById("modalesParaAgregarContainer");
+
+    $.ajax({
+        type:'GET',
+        url: url + "inscripcion/iniciarCuestionario",
+        data: {trayecto:trayectoId},
+        success: [function (response){
+            $("#modalesParaAgregarContainer").html(response);
+            modalContainer.style.display = "flex";
+        }]
+    })
+
+}
 
 
 
